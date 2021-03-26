1e12cb9390a13ffdb1989b769345d3e0de876be7
Alberto <uo266536@uniovi.es>
2021-03-26 10:57:53 +0100

    SDI-2021-305-7.5-Vistas y motores de plantillas.

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -1,30 +1,42 @@
-module.exports = function(app) {
+module.exports = function(app, swig) {
     app.get("/canciones", function(req, res) {
-        let respuesta = "";
+        let canciones = [{
+            "nombre" : "Blank space",
+            "precio" : "1.2"
+        },{
+            "nombre" : "See you again",
+            "precio" : "1.3"
+        },{
+            "nombre" : "Uptown funk",
+            "precio" : "1.1"
+        }
+        ];
 
-        if(req.query.nombre != null)
-            respuesta +=  'Nombre: ' + req.query.nombre + '<br>';
-
-        if(typeof (req.query.autor) != "undefined")
-            respuesta += 'Autor: '+ req.query.autor;
-
-        app.get('/suma', function(req, res) {
-            let respuesta = parseInt(req.query.num1) + parseInt(req.query.num2);
-            res.send(String(respuesta));
+        let respuesta = swig.renderFile('views/btienda.html', {
+            vendedor : 'Tienda de canciones',
+            canciones : canciones
         });
 
-        app.get('/canciones/:id', function(req, res) {
-            let respuesta = 'id: ' + req.params.id;
-            res.send(respuesta);
-        });
-        app.get('/canciones/:genero/:id', function(req, res) {
-            let respuesta = 'id: ' + req.params.id + '<br>'
-                + 'Género: ' + req.params.genero;
-            res.send(respuesta);
-        });
+        res.send(String(respuesta));
+    });
 
+    app.get('/canciones/agregar', function (req, res) {
+        let respuesta = swig.renderFile('views/bagregar.html', {
+
+        });
+        res.send(respuesta);
+    })
+
+    app.get('/canciones/:id', function(req, res) {
+        let respuesta = 'id: ' + req.params.id;
         res.send(respuesta);
     });
+    app.get('/canciones/:genero/:id', function(req, res) {
+        let respuesta = 'id: ' + req.params.id + '<br>'
+            + 'Género: ' + req.params.genero;
+        res.send(respuesta);
+    });
+
 
     app.post("/cancion", function(req, res) {
         res.send("Canción agregada:" + req.body.nombre + "<br>"
